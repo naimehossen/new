42e78b4015e2e5185b2f1808d0d7e0a95db9a6f3
naimehossen <n33937407@gmail.com>
2025-06-01 07:57:53 +0600

    html css js
    simple

---
+++ 50event.js
@@ -0,0 +1,60 @@
+
+
+var deatils=document.querySelector("details");
+
+deatils.addEventListener("toggle",function (e) {
+      
+      
+      
+      console.log(e.target.open);
+});
+
+
+
+
+
+
+
+
+
+/*  
+var deatils=document.querySelector("details");
+
+deatils.addEventListener("toggle",function () {
+      
+      
+      
+      console.log("toggle");
+});
+
+window.addEventListener("resize",function () {
+ var width=window.outerWidth;
+var height=window.outerHeight;  
+      
+console.log(`height:${height},width:${width}`);
+});
+ 
+
+window.addEventListener("resize",function () {
+      
+      
+      console.log("resize");
+});
+
+window.addEventListener("scroll",function () {
+      
+      
+      console.log("scroll");
+});
+ window.addEventListener("load",function () {
+      
+      
+      console.log("load");
+});
+
+
+window.addEventListener("unload",function () {
+      
+      
+      console.log("unload");
+}); */
